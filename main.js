a6043928fb68c46a5429c6aee7c9a1a86a77f6a6
Paul Frazee <pfrazee@gmail.com>
2019-02-11 14:11:04 -0600

    Implement webview-freeze bug reproduction

--- main.js
+++ main.js
@@ -1,50 +1,61 @@
 // Modules to control application life and create native browser window
-const {app, BrowserWindow} = require('electron')
+const {URL} = require('url')
+const {app, BrowserWindow, protocol} = require('electron')
+const fs = require('fs')
+const path = require('path')
 
-// Keep a global reference of the window object, if you don't, the window will
-// be closed automatically when the JavaScript object is garbage collected.
 let mainWindow
 
 function createWindow () {
-  // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600})
-
-  // and load the index.html of the app.
+  mainWindow = new BrowserWindow()
   mainWindow.loadFile('index.html')
-
-  // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
-
-  // Emitted when the window is closed.
   mainWindow.on('closed', function () {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
     mainWindow = null
   })
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.on('ready', createWindow)
+app.on('ready', () => {
+
+  protocol.registerStreamProtocol('custom', (request, callback) => {
+    console.log(request.url)
+    var urlp = new URL(request.url)
+
+    if (urlp.pathname === '/index.html') {
+      return callback({
+        statusCode: 200,
+        headers: {
+          'content-type': 'text/html'
+        },
+        data: fs.createReadStream(path.join(__dirname, 'custom', 'index.html'))
+      })
+    }
+    if (urlp.pathname === '/cats.jpg') {
+      return callback({
+        statusCode: 200,
+        headers: {
+          'content-type': 'image/jpeg'
+        },
+        data: fs.createReadStream(path.join(__dirname, 'custom', 'cats.jpg'))
+      })
+    }
+
+    callback({statusCode: 404})
+  }, (error) => {
+    if (error) console.error('Failed to register protocol')
+  })
+
+  createWindow()
+})
 
 // Quit when all windows are closed.
 app.on('window-all-closed', function () {
-  // On macOS it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
   if (process.platform !== 'darwin') {
     app.quit()
   }
 })
 
 app.on('activate', function () {
-  // On macOS it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
   if (mainWindow === null) {
     createWindow()
   }
 })
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
